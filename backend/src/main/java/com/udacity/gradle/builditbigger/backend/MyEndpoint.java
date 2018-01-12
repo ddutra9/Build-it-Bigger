0242685a58f751cf78520286df05bcd90cb8210d
ddutra9 <7Free787>
2018-01-12 00:19:59 -0200

    add endpont para o servidor

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.udacity.gradle.builditbigger.backend;
 
+import com.example.javajokes.Joker;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -30,6 +31,13 @@ public class MyEndpoint {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
+        return response;
+    }
+
+    @ApiMethod(name = "jokes")
+    public MyBean jokes() {
+        MyBean response = new MyBean();
+        response.setData(new Joker().getJoke());
         return response;
     }
 
--- javaJokes/src/main/java/com/example/javajokes/Joker.java
+++ javaJokes/src/main/java/com/example/javajokes/Joker.java
@@ -1,9 +1,15 @@
 package com.example.javajokes;
 
+import java.util.Random;
+
 public class Joker {
 
+    private static String[] jokes = {"Mom, where do tampons go?\n" + "Where the babies come from, darling.\n" +
+            "In the stork?", "What is the difference between a snowman and a snowwoman?\n" +
+            "Snowballs.", " My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away."};
+
     public String getJoke() {
-        return "This is totally a funny joke";
+        return jokes[new Random().nextInt(jokes.length - 1)];
     }
 
 }
